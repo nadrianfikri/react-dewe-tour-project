9864c55a4b3c254c99dfca1b92a9f5abdd9f5b6c
nadrianfikri <ahmad.alfikri.n91@gmail.com>
2021-10-23 22:45:27 +0700

    done income trip

--- src/pages/IncomeTrip.js
+++ src/pages/IncomeTrip.js
@@ -1,5 +1,27 @@
+import { Card2 } from '../components/Card';
+import dataDestinations from '../assets/destination.json';
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+
 function IncomeTrip() {
-  return <div></div>;
+  return (
+    <div className="pt-36 bg-gray-100 ">
+      <Navbar class="bg-navbar" />
+
+      <main className="md:container mx-auto overflow-auto pb-36 ">
+        <div className="flex justify-between px-6 pb-10">
+          <h1 className="text-4xl font-bold">Income Trip</h1>
+          <button className="bg-yellow-400 hover:bg-yellow-500 py-2 px-10 text-white font-bold rounded-md">Add Trip</button>
+        </div>
+        <div className="group-content flex justify-center items-center flex-wrap">
+          {dataDestinations.map((data) => (
+            <Card2 title={data.name} img={data.image} capacity={data.capacity} price={data.price} country={data.country} />
+          ))}
+        </div>
+      </main>
+      <Footer />
+    </div>
+  );
 }
 
 export default IncomeTrip;
