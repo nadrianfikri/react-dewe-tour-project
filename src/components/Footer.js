61dfc376583101a8f33263ecf7fad35db50364a1
nadrianfikri <ahmad.alfikri.n91@gmail.com>
2021-11-03 23:42:24 +0700

    responsive footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,9 +1,9 @@
 import image from '../assets/images/leaf.png';
 function Footer() {
   return (
-    <div id="footer" className=" relative text-center bg-yellow-400 text-white text-lg py-6 mt-20">
-      <p>Copyright @ 2020 Dewe Tour - Fikri Nadrian All Rights reserved</p>
-      <img className="absolute bottom-0 right-0" src={image} alt="palm" />
+    <div id="footer" className="relative text-center bg-yellow-400  text-white text-md md:text-lg py-4 md:py-6 mt-20">
+      <p className="w-3/4 md:w-full pl-10 md:pl-0">Copyright @ 2021 Dewe Tour - Fikri Nadrian All Rights reserved</p>
+      <img className="absolute bottom-0 right-0 md:h-auto h-full" src={image} alt="palm" />
     </div>
   );
 }
