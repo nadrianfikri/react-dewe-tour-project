4ff19b609a16da315f4240eeccce4ec4128ea8ac
nadrianfikri <ahmad.alfikri.n91@gmail.com>
2021-11-04 18:41:53 +0700

    fixed authContext

--- src/context/authContext.js
+++ src/context/authContext.js
@@ -1,57 +1,39 @@
 import { createContext, useReducer } from 'react';
-import users from '../assets/user.json';
 
 const initialValue = {
   isLogin: false,
-  user: users,
+  user: {},
 };
-
-// localStorage.setItem('user', JSON.stringify(initialValue));
 
 export const AuthContext = createContext();
 
 function reducer(state, action) {
   const { type, payload } = action;
+
   switch (type) {
-    case 'LOGIN':
-      localStorage.setItem(
-        'user',
-        JSON.stringify({
-          isLogin: true,
-          user: payload,
-        })
-      );
+    case 'AUTH_SUCCESS':
+    case 'LOGIN_SUCCESS':
+      localStorage.setItem('token', payload.token);
 
       return {
         isLogin: true,
         user: payload,
       };
 
+    case 'AUTH_ERROR':
     case 'LOGOUT':
-      // const user = JSON.parse(localStorage.getItem('user'));
-      localStorage.setItem(
-        'user',
-        JSON.stringify({
-          isLogin: false,
-          user: initialValue.user,
-        })
-      );
-      return initialValue;
+      localStorage.removeItem('token');
 
-    case 'AUTH':
-      const loginState = JSON.parse(localStorage.getItem('user'));
-      return loginState ? loginState : initialValue;
+      return {
+        isLogin: false,
+        user: {},
+      };
 
     default:
       throw new Error("type doesn't match cases");
   }
 }
 
-/** this is wrapper component that will use to be parent component of children that need to
- * consume the state globally
- * @param {*} children
- * @returns
- */
 const AuthContextProvider = ({ children }) => {
   const [state, dispatch] = useReducer(reducer, initialValue);
 
