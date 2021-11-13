311dd118f60518e99dab85040590a26adb645230
nadrianfikri <ahmad.alfikri.n91@gmail.com>
2021-11-14 01:32:41 +0700

    done notification for admin

--- src/pages/IncomeTrip.js
+++ src/pages/IncomeTrip.js
@@ -31,9 +31,14 @@ function IncomeTrip() {
       <main className="md:container mx-auto overflow-auto pb-36 ">
         <div className="flex justify-between px-6 pb-10">
           <h1 className="text-4xl font-bold">Income Trip</h1>
-          <Link to="add-trip" className="bg-yellow-400 hover:bg-yellow-500 py-2 px-10 text-white font-bold rounded-md">
-            Add Trip
-          </Link>
+          <div className="flex gap-4">
+            <button type="button" className="bg-yellow-400 hover:bg-yellow-500 py-2 px-4 text-white font-bold rounded-md">
+              Add Country
+            </button>
+            <Link to="add-trip" className="bg-yellow-400 hover:bg-yellow-500 py-2 px-10 text-white font-bold rounded-md">
+              Add Trip
+            </Link>
+          </div>
         </div>
         <div className="group-content flex justify-center items-center flex-wrap">
           {trips.map((data) => (
